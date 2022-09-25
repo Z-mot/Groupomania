c04daa3eef824c83f833319f630df5c8efc0b56f
Zmot <toto95270@hotmail.fr>
2022-09-25 16:40:57 +0200

    Import d'express et ajout du middleware CORS dans le fichier app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,47 +1,21 @@
-const http = require('http');
-const app = require('./app');
+import express from'express';
 
-const normalizePort = val => {
-    const port = parseInt(val, 10);
+import Router from "./routes/routes.js";
 
-    if (isNaN(port)) {
-        return val;
-    }
-    if (port >= 0) {
-        return port;
-    }
-    return false;
-};
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+const app = express();
 
-const errorHandler = error => {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges.');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use.');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-};
-
-const server = http.createServer(app);
-
-server.on('error', errorHandler);
-server.on('listening', () => {
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-    console.log('Listening on ' + bind);
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
 
-server.listen(port);
+// use express json
+app.use(express.json());
+
+// use router
+app.use(Router);
+
+
+export default app;
