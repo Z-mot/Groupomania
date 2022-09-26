6c5b67c9ac9048dab0535bcfc3b5924a06788a41
Zmot <toto95270@hotmail.fr>
2022-09-26 15:14:05 +0200

    database.js commenté

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,6 +1,7 @@
+// Importation de mysql pour pouvoir connecter le backend à la BDD
 import mysql from "mysql";
 
-// create the connection to database
+// Création de la connexion avec la BDD
 const db = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
