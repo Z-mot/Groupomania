0db77ecc91d391819c99598dbeadf7a8a13b2d0b
Zmot <toto95270@hotmail.fr>
2022-09-26 19:59:45 +0200

    Correction de la méthode savePost pour que la création d'un Post sans image puisse-être possible + code commenté

--- Frontend/groupomania/src/components/AddPost.vue
+++ Frontend/groupomania/src/components/AddPost.vue
@@ -14,7 +14,7 @@
 	  <div class="field">
       <label class="label">Image</label>
       <div class="control">
-        <input type="file" ref="file" @change="previewFiles" >
+        <input type="file" ref="file" id="file" @change="previewFiles" >
       </div>
     </div>
     <div class="control">
@@ -37,36 +37,55 @@ export default {
       message: "",
     };
   },
+
+  
   methods: {
-	previewFiles() {
+    // Méthode Vue pour afficher le nom du fichier avant l'upload
+	  previewFiles() {
 	  this.file = document.querySelector('input[type=file]')['files'][0];
     },
+
+    //Méthode Vue pour déterminer si les données à upload seront que du texte ou des images et du texte
     async savePost() {
       try {
-		  let base64String ="";
-		  var vm = this;
-		  var reader = new FileReader();
-		  reader.onload = function () {
-			base64String = reader.result.replace("data:", "").replace(/^.+,/, "");	  
-			base64String = "data:image/png;base64,"+base64String ;	  
-			var dataPost = {
-									post_name: vm.postName,
-									post_image: base64String,
-                  user_id: localStorage.getItem("user_id"),
-								  }
-			vm.postData(dataPost);
-		  }
-		  reader.readAsDataURL(this.file);
+        // si le post n'a pas d'image, on upload que le texte
+        if (document.getElementById("file").value == "" ) {
+          let vm = this;
+          let dataPost = {
+                      post_name: vm.postName,
+                      user_id: localStorage.getItem("user_id"),
+                      }
+          vm.postData(dataPost);
+          } else {
+            // sinon on upload le texte et l'image (le texte peut être nul)
+            let base64String ="";
+            let vm = this;
+            let reader = new FileReader();
+            reader.onload = function () {
+            base64String = reader.result.replace("data:", "").replace(/^.+,/, "");	  
+            base64String = "data:image/png;base64,"+base64String; 
+            let dataPost = {
+                      post_name: vm.postName,
+                      post_image: base64String,
+                      user_id: localStorage.getItem("user_id"),
+                      }
+            vm.postData(dataPost);
+            }
+            reader.readAsDataURL(this.file);
+          }
       } catch (err) {
         console.log(err);
       }
     },
+
+    // Méthode Vue pour Upload les données dans la BDD
     async postData(params) {
       let webApiUrl = "http://localhost:5000/posts";
       let tokenStr = localStorage.getItem("token");
       await axios.post(webApiUrl, params, { headers: { "Authorization": `Bearer ${tokenStr}` } });
       this.postName = "";
       this.postImage = "";
+      // On envoi l'utilisateur sur la page d'accueil
       this.$router.push("/home");
     },
   },
