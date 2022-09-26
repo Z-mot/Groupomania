e8f2e3f0321d425a1a4a40c922baf4f1ff62d950
Zmot <toto95270@hotmail.fr>
2022-09-26 15:14:18 +0200

    app.js du backend commenté

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,7 @@ import Router from "./routes/routes.js";
 
 const app = express();
 
+//utilisation d'un middleware permettant de contourner les erreurs CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -11,7 +12,9 @@ app.use((req, res, next) => {
     next();
 });
 
-// use express json
+/*utilisation de la méthode app.use prenant en paramètre le middleware express.json afin
+de rendre disponible le body directement dans l'objet req pour toute les requêtes ayant
+un content-type : application/json*/
 app.use(express.json());
 
 // use router
